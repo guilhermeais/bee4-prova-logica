50bba63b34177ff1b30cec8b6f9375ea89936456
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-03-11 11:25:10 -0300

    feat(CepService): implements CepService interface with ViaCep implementation

---
+++ utils/gateway/viacep-cep-service.js
@@ -0,0 +1,50 @@
+import { Address } from '../interfaces/address.js'
+import { CepService } from '../interfaces/cep-service.js'
+import { errors } from '../constants/errors.js'
+import axios from 'axios'
+/**
+ * @implements {CepService}
+ */
+export class ViaCepCepService extends CepService {
+  constructor() {
+    super()
+    this.api = axios.create({
+      baseURL: 'https://viacep.com.br/ws/',
+    })
+  }
+  /**
+   * @param {string} cep
+   * @returns {Promise<Address>}
+   */
+  async findAddressByCep(cep) {
+    const { data, status } = await this.api.get(`${cep}/json/`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+       
+    })
+    
+    const isClientError = status >= 400 && status < 500
+
+    if (isClientError) {
+      throw new Error(errors.INVALID_CEP)
+    }
+    console.info('viaCepCepService: ', data)
+
+    const address = new Address({
+      cep: data.cep,
+      city: data.localidade,
+      neighborhood: data.bairro,
+      country: 'Brasil',
+      state: data.uf,
+      street: data.logradouro,
+      complement: data.complemento,
+      gia: data.gia,
+      ibge: data.ibge,
+      ddd: data.ddd,
+    })
+
+
+    return address
+  }
+}
